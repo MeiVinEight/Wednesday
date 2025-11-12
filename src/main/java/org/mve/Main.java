4c810fa470f42be6c1c50c659e11b3686ec2633d
MeiVinEight <MeiVinEight@outlook.com>
2025-11-12 17:24:15 +0800

    重置项目

--- src/main/java/org/mve/Main.java
+++ src/main/java/org/mve/Main.java
@@ -1,33 +1,10 @@
 package org.mve;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Main
 {
 	public static void main(String[] args)
 	{
-		int shortId = Integer.parseInt(args[0]);
-		String cookie;
-		try (FileInputStream fin = new FileInputStream("cookie"))
-		{
-			Scanner sc = new Scanner(fin);
-			cookie = sc.nextLine();
-		}
-		catch (FileNotFoundException e)
-		{
-			Wednesday.message("Cookie file not found, input cookie: ");
-			Scanner sc = new Scanner(System.in);
-			cookie = sc.nextLine();
-		}
-		catch (IOException e)
-		{
-			Wednesday.message(null, e);
-			return;
-		}
-		Wednesday danmu = new Wednesday(cookie, shortId);
-		Runtime.getRuntime().addShutdownHook(new Thread(danmu::close));
+		Wednesday wednesday = new Wednesday();
+		Runtime.getRuntime().addShutdownHook(new Thread(wednesday::close));
 	}
 }

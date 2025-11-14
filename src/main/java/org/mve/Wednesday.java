c30c5d8739fb72d1e1a2f58af0081ec2f3f27a7f
MeiVinEight <MeiVinEight@outlook.com>
2025-11-14 09:51:24 +0800

    ✨连接OneBot

--- src/main/java/org/mve/Wednesday.java
+++ src/main/java/org/mve/Wednesday.java
@@ -1,19 +1,45 @@
 package org.mve;
 
+import net.mamoe.mirai.Bot;
+import top.mrxiaom.overflow.BotBuilder;
+
 public class Wednesday extends Synchronize
 {
-	private final SynchronizeNET synchronize = new SynchronizeNET();
+	public static final String SID = "Wednesday";
+	public final SynchronizeNET synchronize = new SynchronizeNET();
+	public static final WednesdayLogger LOGGER = new WednesdayLogger(SID, Configuration.LOG_LEVEL);
+	private final Bot QQ;
 
 	public Wednesday()
 	{
 		this.synchronize.offer(this);
 		new Thread(this.synchronize).start();
+		this.QQ = BotBuilder.positive(Configuration.ONEBOT_WS_FORWARD)
+			.token(Configuration.ONEBOT_TOKEN)
+			.modifyBotConfiguration(config ->
+			{
+				config.setBotLoggerSupplier(bot -> LOGGER);
+				config.setNetworkLoggerSupplier(bot -> new WednesdayLogger("Network", Configuration.LOG_LEVEL));
+				config.setReconnectionRetryTimes(2);
+			})
+			.retryTimes(0)
+			.retryRestMills(-1)
+			.overrideLogger(LOGGER)
+			.connect();
+		if (QQ == null)
+		{
+			this.close();
+			System.exit(0);
+		}
 	}
 
 	public void close()
 	{
+		LOGGER.info("Wednesday 关闭");
 		this.cancel();
 		this.synchronize.close();
+		if (this.QQ != null)
+			this.QQ.close();
 	}
 
 	@Override

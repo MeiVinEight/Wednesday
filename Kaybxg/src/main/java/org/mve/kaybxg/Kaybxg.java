364703c555c6c7dd7eb1453dd89b9874ec3e3085
MeiVinEight <MeiVinEight@outlook.com>
2025-11-17 19:04:00 +0800

    ✨简单启动Wednesday

---
+++ Kaybxg/src/main/java/org/mve/kaybxg/Kaybxg.java
@@ -0,0 +1,65 @@
+package org.mve.kaybxg;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class Kaybxg
+{
+	public static boolean running = true;
+	public static final Object WAIT = new Object();
+
+	public static void main(String[] args)
+	{
+		while (running)
+		{
+			running = false;
+			try
+			{
+				load();
+			}
+			catch (Throwable e)
+			{
+				e.printStackTrace(System.out);
+			}
+		}
+	}
+
+	private static void load() throws Throwable
+	{
+		File file = new File("libs");
+		URL[] urls = Stream
+			.of(Objects.requireNonNull(file.listFiles()))
+			.map(Kaybxg::toURL)
+			.filter(Objects::nonNull)
+			.toArray(URL[]::new);
+		URLClassLoader loader = new URLClassLoader("Kaybxg", urls, Kaybxg.class.getClassLoader());
+		Class<?> clazz = loader.loadClass("org.mve.Main");
+		Method method = clazz.getDeclaredMethod("main", String[].class);
+		method.setAccessible(true);
+		method.invoke(null, (Object) new String[0]);
+		loader.close();
+	}
+
+	private static URL toURL(File f)
+	{
+		try
+		{
+			return f.toURI().toURL();
+		}
+		catch (Throwable e)
+		{
+			exception(e);
+		}
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T extends Throwable> void exception(Throwable t) throws T
+	{
+		throw (T) t;
+	}
+}

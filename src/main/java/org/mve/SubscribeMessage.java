8ff14041d94efa3d1f2f1737d23ad41101f217ec
MeiVinEight <MeiVinEight@outlook.com>
2026-01-17 13:36:19 +0800

    ✨添加戳一戳发表情包功能

--- src/main/java/org/mve/SubscribeMessage.java
+++ src/main/java/org/mve/SubscribeMessage.java
@@ -1,40 +1,47 @@
 package org.mve;
 
+import net.mamoe.mirai.contact.Contact;
 import net.mamoe.mirai.event.Event;
 import net.mamoe.mirai.event.ListeningStatus;
 import net.mamoe.mirai.event.events.BotOfflineEvent;
 import net.mamoe.mirai.event.events.MessageEvent;
+import net.mamoe.mirai.event.events.NudgeEvent;
+import net.mamoe.mirai.message.data.Image;
 import net.mamoe.mirai.message.data.MessageChain;
 import net.mamoe.mirai.message.data.PlainText;
+import org.mve.vo.Facing;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Random;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.function.Function;
 
 public class SubscribeMessage extends Synchronize implements Function<Event, ListeningStatus>
 {
 	private final Wednesday wednesday;
-	private final Queue<MessageEvent> queue = new ConcurrentLinkedQueue<>();
+	private final Queue<Event> queue = new ConcurrentLinkedQueue<>();
 	private final Map<String, Function<MessageEvent, Boolean>> listeners = new HashMap<>();
+	private final SimpleMapper<Facing> facing;
+	private final Random random = new Random();
 
 	public SubscribeMessage(Wednesday wednesday)
 	{
 		this.wednesday = wednesday;
+		String mysqlUrl = "jdbc:mysql://" + Configuration.MYSQL_HOST + ':' + Configuration.MYSQL_PORT + "/COFFEE";
+		this.facing = new SimpleMapper<>(new Database(mysqlUrl, Configuration.MYSQL_USERNAME, Configuration.MYSQL_PASSWORD));
 	}
 
 	@Override
 	public ListeningStatus apply(Event event)
 	{
-		if (event instanceof MessageEvent me)
-			return this.apply(me);
-		else if (event instanceof BotOfflineEvent)
-			this.wednesday.close();
-		return ListeningStatus.LISTENING;
+		Wednesday.LOGGER.debug(event.toString());
+		return this.push(event);
 	}
 
-	public ListeningStatus apply(MessageEvent event)
+	public ListeningStatus push(Event event)
 	{
 		// Add event to queue and handle event in another thread
 		this.queue.add(event);
@@ -49,21 +56,47 @@ public class SubscribeMessage extends Synchronize implements Function<Event, Lis
 	@Override
 	public void run()
 	{
-		MessageEvent event = queue.poll();
+		Event event = queue.poll();
 		if (event == null)
 			return;
-		MessageChain msg = event.getMessage();
-		if (!(msg.get(1) instanceof PlainText text))
-			return;
-		String content = text.getContent();
-		if (!content.startsWith(Configuration.COMMAND_PREFIX))
-			return;
-		content = content.substring(1);
-		String contentWithoutPrefix = content;
-		this.listeners.forEach((pfx, listener) ->
+		if (event instanceof MessageEvent messageEvent)
 		{
-			if (contentWithoutPrefix.startsWith(pfx))
-				listener.apply(event);
-		});
+			MessageChain msg = messageEvent.getMessage();
+			if (!(msg.get(1) instanceof PlainText text))
+				return;
+			String content = text.getContent();
+			if (!content.startsWith(Configuration.COMMAND_PREFIX))
+				return;
+			content = content.substring(1);
+			String contentWithoutPrefix = content;
+			this.listeners.forEach((pfx, listener) ->
+			{
+				if (contentWithoutPrefix.startsWith(pfx))
+					listener.apply(messageEvent);
+			});
+		}
+		if (event instanceof BotOfflineEvent)
+			this.wednesday.close();
+		if (event instanceof NudgeEvent nudge)
+		{
+			if (nudge.getTarget().getId() == nudge.getBot().getId())
+			{
+				int count = this.facing.count("FACING");
+				Wednesday.LOGGER.debug("{} Facings", count);
+				Facing fac = new Facing();
+				fac.ID = random.nextInt(count);
+				fac = this.facing.primary(fac);
+				String url = Configuration.FILE_SERVER + "/image/" + fac.NAME;
+				Image image = Image.fromId(url);
+				if (url.startsWith("file:///"))
+				{
+					File file = new File(url.substring("file:///".length()));
+					if (!file.isFile())
+						return;
+					image = Contact.uploadImage(nudge.getSubject(), file);
+				}
+				nudge.getSubject().sendMessage(image);
+			}
+		}
 	}
 }
---
+++ src/main/java/org/mve/vo/Facing.java
@@ -0,0 +1,11 @@
+package org.mve.vo;
+
+import javax.persistence.Table;
+
+@Table("FACING")
+public class Facing
+{
+	public int ID;
+	public String SHA1;
+	public String NAME;
+}

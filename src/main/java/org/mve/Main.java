09faeab4f8f1b4f35d2161415332a886399ba8f0
MeiVinEight <MeiVinEight@outlook.com>
2026-01-20 14:42:42 +0800

    🎨删除线程关闭代码

--- src/main/java/org/mve/Main.java
+++ src/main/java/org/mve/Main.java
@@ -1,15 +1,8 @@
 package org.mve;
 
-import kotlinx.coroutines.DefaultExecutor;
-import kotlinx.coroutines.scheduling.CoroutineScheduler;
-import kotlinx.coroutines.scheduling.CoroutineSchedulerKt;
 import net.mamoe.mirai.utils.SimpleLogger;
 import org.mve.logging.FileLogger;
 import org.mve.logging.LoggerManager;
-
-import java.util.Objects;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 public class Main
 {
@@ -17,7 +10,6 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		System.setProperty("kotlinx.coroutines.scheduler.keep.alive.sec", "1");
 		LoggerManager.register(FileLogger.INSTANCE);
 		Wednesday.LOGGER.info(LoggerMessage.LOG_WEDNESDAY_STARTUP);
 		Wednesday.LOGGER.info(LoggerMessage.LOG_WEDNESDAY_STARTUP_PATCHING);
@@ -49,45 +41,5 @@ public class Main
 		{
 			Wednesday.LOGGER.error(e);
 		}
-		Thread current = Thread.currentThread();
-		Thread[] threads = new Thread[64];
-		int count;
-		for (;;)
-		{
-			ThreadGroup group = Thread.currentThread().getThreadGroup();
-			count = group.enumerate(threads);
-			int nonDaemon = Stream.of(threads)
-				.filter(Objects::nonNull)
-				.filter(Predicate.not(Predicate.isEqual(current)))
-				.filter(Predicate.not(Thread::isDaemon))
-				.toArray()
-				.length;
-			if (nonDaemon == 0)
-				break;
-			synchronized (WAITING)
-			{
-				try
-				{
-					WAITING.wait(1000);
-				}
-				catch (InterruptedException e)
-				{
-					Wednesday.LOGGER.warn("", e);
-				}
-			}
-		}
-		DefaultExecutor.INSTANCE.shutdown();
-		for (int i = 0; i < count; i++)
-		{
-			Thread key =  threads[i];
-			Wednesday.LOGGER.trace("Interrupt {}", key.getName());
-			if (key.isDaemon())
-			{
-				if (CoroutineSchedulerKt.isSchedulerWorker(key))
-					((CoroutineScheduler.Worker) key).state = CoroutineScheduler.WorkerState.TERMINATED;
-				key.interrupt();
-			}
-		}
-		FileLogger.INSTANCE.close();
 	}
 }

0415ac1305c73533d6412ff8abf20132822ddb72
MeiVinEight <MeiVinEight@outlook.com>
2026-01-20 15:14:28 +0800

    🎨移动表情下载逻辑到单独类中

--- src/main/java/org/mve/SubscribeMessage.java
+++ src/main/java/org/mve/SubscribeMessage.java
@@ -9,16 +9,10 @@ import net.mamoe.mirai.event.events.NudgeEvent;
 import net.mamoe.mirai.message.data.Image;
 import net.mamoe.mirai.message.data.MessageChain;
 import net.mamoe.mirai.message.data.PlainText;
-import net.mamoe.mirai.message.data.SingleMessage;
+import org.mve.service.NudgeFacing;
 import org.mve.vo.Facing;
-import top.mrxiaom.overflow.internal.message.data.WrappedImage;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Queue;
@@ -68,67 +62,10 @@ public class SubscribeMessage extends Synchronize implements Function<Event, Lis
 			return;
 		if (event instanceof MessageEvent messageEvent)
 		{
+			NudgeFacing.capture(messageEvent);
+
+
 			MessageChain chain = messageEvent.getMessage();
-			for (SingleMessage msg : chain)
-			{
-				if (msg instanceof WrappedImage wimg)
-				{
-					if (wimg.getJson() == null)
-						continue;
-					Json data = Json.resolve(wimg.getJson());
-					Wednesday.LOGGER.verbose(data.stringify());
-					String summary = data.string("summary");
-					if (summary == null || summary.isEmpty())
-						continue;
-
-					String fileName = data.string("file");
-					Facing fac = new Facing();
-					fac.NAME = fileName;
-					if (this.facing.count(fac) > 0)
-						continue;
-
-					String url = data.string("url");
-					try
-					{
-						URL uploadUrl = new URL("http://meivi.net:83/upload");
-						HttpURLConnection conn = (HttpURLConnection) uploadUrl.openConnection();
-						conn.setRequestProperty("Download-From", url);
-						conn.setRequestProperty("Download-To", "image/" + fileName);
-
-						int respCode = conn.getResponseCode();
-						if (respCode != HttpURLConnection.HTTP_OK)
-						{
-							Wednesday.LOGGER.error("http://meivi.net:83/upload: {}", conn.getResponseMessage());
-							continue;
-						}
-
-						Json body;
-						try (InputStream in = conn.getInputStream())
-						{
-							ByteArrayOutputStream out = new ByteArrayOutputStream();
-							in.transferTo(out);
-							body = Json.resolve(out.toString());
-						}
-
-						respCode = body.number("code").intValue();
-						String message = body.string("message");
-						if (respCode != 0 && respCode != 5)
-						{
-							Wednesday.LOGGER.warn("{}: {}", respCode, message);
-							continue;
-						}
-
-						data = body.get("data");
-						fac.SHA1 = data.string("SHA1");
-						this.facing.insert(fac);
-
-					}
-					catch (IOException e)
-					{
-						throw new RuntimeException(e);
-					}
-				}
-			}
 
 
 			if (!(chain.get(1) instanceof PlainText text))
---
+++ src/main/java/org/mve/service/NudgeFacing.java
@@ -0,0 +1,95 @@
+package org.mve.service;
+
+import net.mamoe.mirai.event.events.MessageEvent;
+import net.mamoe.mirai.message.data.MessageChain;
+import net.mamoe.mirai.message.data.SingleMessage;
+import org.mve.Configuration;
+import org.mve.Database;
+import org.mve.Json;
+import org.mve.SimpleMapper;
+import org.mve.Wednesday;
+import org.mve.vo.Facing;
+import top.mrxiaom.overflow.internal.message.data.WrappedImage;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NudgeFacing
+{
+	private static final SimpleMapper<Facing> FACING;
+
+	public static void capture(MessageEvent messageEvent)
+	{
+		MessageChain chain = messageEvent.getMessage();
+		for (SingleMessage msg : chain)
+		{
+			if (!(msg instanceof WrappedImage wimg))
+				continue;
+
+			if (wimg.getJson() == null)
+				continue;
+			Json data = Json.resolve(wimg.getJson());
+			Wednesday.LOGGER.verbose(data.stringify());
+			String summary = data.string("summary");
+			if (summary == null || summary.isEmpty())
+				continue;
+
+			String fileName = data.string("file");
+			Facing fac = new Facing();
+			fac.NAME = fileName;
+			if (FACING.count(fac) > 0)
+				continue;
+
+			String url = data.string("url");
+			try
+			{
+				String urlText = Configuration.COFFEE_SERVER + "/upload";
+				URL uploadUrl = new URL(urlText);
+				HttpURLConnection conn = (HttpURLConnection) uploadUrl.openConnection();
+				conn.setRequestProperty("Download-From", url);
+				conn.setRequestProperty("Download-To", "image/" + fileName);
+
+				int respCode = conn.getResponseCode();
+				if (respCode != HttpURLConnection.HTTP_OK)
+				{
+					Wednesday.LOGGER.error("{}: {}", urlText, conn.getResponseMessage());
+					continue;
+				}
+
+				Json body;
+				try (InputStream in = conn.getInputStream())
+				{
+					ByteArrayOutputStream out = new ByteArrayOutputStream();
+					in.transferTo(out);
+					body = Json.resolve(out.toString());
+				}
+
+				respCode = body.number("code").intValue();
+				String message = body.string("message");
+				if (respCode != 0 && respCode != 5)
+				{
+					Wednesday.LOGGER.warn("{}: {}", respCode, message);
+					continue;
+				}
+
+				data = body.get("data");
+				fac.SHA1 = data.string("SHA1");
+				FACING.insert(fac);
+
+			}
+			catch (IOException e)
+			{
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	static
+	{
+		String mysqlUrl = "jdbc:mysql://" + Configuration.MYSQL_HOST + ':' + Configuration.MYSQL_PORT + "/COFFEE";
+		FACING = new SimpleMapper<>(new Database(mysqlUrl, Configuration.MYSQL_USERNAME, Configuration.MYSQL_PASSWORD));
+	}
+}

452609dea8efabf9a61c3c9b14e309a52aded33e
MeiVinEight <MeiVinEight@outlook.com>
2026-02-12 23:17:46 +0800

    ✨连接管理器

---
+++ src/main/java/org/mve/ConnectionWednesday.java
@@ -0,0 +1,109 @@
+package org.mve;
+
+import org.mve.uni.Json;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Table(ConnectionWednesday.TABLE_NAME)
+public class ConnectionWednesday
+{
+	public static final String TABLE_NAME = "CONNECTION";
+	public static final String KEY_NAME = "name";
+	public static final String KEY_URL = "url";
+	public static final String KEY_TOKEN = "token";
+	public Integer ID;
+	public String NAME;
+	public String URL;
+	public String TOKEN;
+	@Column(exclude = true)
+	public Wednesday connection;
+
+	public ConnectionWednesday()
+	{
+		this(null, null, null);
+	}
+
+	public ConnectionWednesday(String name, String url, String token)
+	{
+		this(null, name, url, token);
+	}
+
+	public ConnectionWednesday(Integer id, String name, String url, String token)
+	{
+		this.ID = id;
+		this.NAME = name;
+		this.URL = url;
+		this.TOKEN = (token != null) ? token : "";
+	}
+
+	public Wednesday connect()
+	{
+		if (this.connection != null)
+			return this.connection;
+		return this.connection = new Wednesday(this.URL, this.TOKEN);
+	}
+
+	public void close()
+	{
+		if (this.connection != null)
+		{
+			try
+			{
+				this.connection.close();
+			}
+			catch (Throwable t)
+			{
+				Wednesday.LOGGER.error(t);
+			}
+			this.connection = null;
+		}
+	}
+
+	public Json data()
+	{
+		Json json = new Json();
+		json.set(KEY_NAME, this.NAME);
+		json.set(KEY_URL, this.URL);
+		json.set(KEY_TOKEN, this.TOKEN);
+		return json;
+	}
+
+	public Json info()
+	{
+		if (this.connection == null)
+			return null;
+		Json info = new Json();
+		info.set(KEY_NAME, this.NAME);
+		info.set(KEY_URL, this.URL);
+		info.set(KEY_TOKEN, this.TOKEN);
+		Json conn = new Json();
+		conn.set("id", this.connection.QQ.getId());
+		conn.set("name", this.connection.QQ.getNick());
+		info.set("conn", conn);
+		return info;
+	}
+
+	public static ConnectionWednesday resolve(Json data)
+	{
+		String name = data.string(KEY_NAME);
+		String url = data.string(KEY_URL);
+		String token = data.string(KEY_TOKEN);
+		return new ConnectionWednesday(null, name, url, token);
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (o == this) return true;
+		if (!(o instanceof ConnectionWednesday that)) return false;
+		return Objects.equals(NAME, that.NAME) && Objects.equals(URL, that.URL) && Objects.equals(TOKEN, that.TOKEN);
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(NAME, URL, TOKEN);
+	}
+}
---
+++ src/main/java/org/mve/WednesdayManager.java
@@ -0,0 +1,88 @@
+package org.mve;
+
+import org.mve.data.Creation;
+import org.mve.data.Database;
+import org.mve.data.Query;
+import org.mve.data.SimpleMapper;
+import org.mve.logging.LoggerManager;
+import org.slf4j.Logger;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class WednesdayManager
+{
+	private static final Logger LOGGER = LoggerManager.create("Manager");
+	public final Map<String, ConnectionWednesday> connection = new ConcurrentHashMap<>();
+	private final SimpleMapper<ConnectionWednesday> data;
+
+	public WednesdayManager(String path)
+	{
+		path = "jdbc:sqlite:" + path;
+		this.data = new SimpleMapper<>(new Database(path, null, null), ConnectionWednesday.class);
+		boolean created = new Creation("CONNECTION")
+			.db(Query.TYPE_SQLITE)
+			.column("ID", "INTEGER")
+			.increment()
+			.primary()
+			.column("NAME", "VARCHAR(64)")
+			.notnull()
+			.unique()
+			.column("URL", "VARCHAR(64)")
+			.notnull()
+			.column("TOKEN", "VARCHAR(64)")
+			.query(this.data);
+		if (created)
+			LOGGER.info("创建连接表 CONNECTION");
+	}
+
+	public void set(ConnectionWednesday connection)
+	{
+		if (connection.NAME == null)
+			return;
+
+		String name = connection.NAME;
+		ConnectionWednesday conn;
+		boolean equals = false;
+
+		if (((conn = this.connection.get(name)) != null) && !(equals = conn.equals(connection)))
+			conn.close();
+
+		if (equals)
+			return;
+
+		int count = this.data.update(connection, "NAME");
+		if (count == 0)
+			this.data.insert(connection);
+		conn = this.data.select(ConnectionWednesday.class, "NAME", name).get(0);
+		this.connection.put(name, conn);
+	}
+
+	public ConnectionWednesday get(String name)
+	{
+		if (name == null)
+			return null;
+		ConnectionWednesday conn = this.connection.get(name);
+		if (conn != null)
+			return conn;
+		List<ConnectionWednesday> conns = this.data.select(ConnectionWednesday.class, "NAME", name);
+		if (conns == null || conns.isEmpty())
+			return null;
+		conn = conns.get(0);
+		this.connection.put(name, conn);
+		return conn;
+	}
+
+	public ConnectionWednesday remove(String name)
+	{
+		ConnectionWednesday conn = this.connection.remove(name);
+		if (conn != null)
+			conn.close();
+		List<ConnectionWednesday> list = this.data.select(ConnectionWednesday.class, "NAME", name);
+		if (list != null && !list.isEmpty())
+			conn = list.get(0);
+		this.data.delete(ConnectionWednesday.class, "NAME", name);
+		return conn;
+	}
+}

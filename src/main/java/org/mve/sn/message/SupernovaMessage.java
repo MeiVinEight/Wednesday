e50001126bae4ec4dbae7a4cd6742f3f028b9292
MeiVinEight <MeiVinEight@outlook.com>
2026-06-26 19:17:10 +0800

    🎨消息解析遵循开闭原则

--- src/main/java/org/mve/sn/message/SupernovaMessage.java
+++ src/main/java/org/mve/sn/message/SupernovaMessage.java
@@ -1,5 +1,6 @@
 package org.mve.sn.message;
 
+import kotlin.Lazy;
 import net.mamoe.mirai.message.data.Face;
 import net.mamoe.mirai.message.data.LightApp;
 import net.mamoe.mirai.message.data.Message;
@@ -7,13 +8,22 @@ import net.mamoe.mirai.message.data.MessageChain;
 import net.mamoe.mirai.message.data.MessageChainBuilder;
 import net.mamoe.mirai.message.data.PlainText;
 import net.mamoe.mirai.message.data.SingleMessage;
+import net.mamoe.mirai.utils.MiraiLogger;
 import org.jetbrains.annotations.NotNull;
 import org.mve.sn.SupernovaAPI;
 import org.mve.sn.core.Supernova;
 import org.mve.uni.Json;
+import org.mve.uni.LazyJVM;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 public class SupernovaMessage implements Message, SingleMessage
 {
+	private static final Map<String, Function<Json, SingleMessage>> DESERIALIZERS = new HashMap<>();
+	private static final Lazy<Function<Json, SingleMessage>> UNKNOWN_MESSAGE = new LazyJVM<>(() -> SupernovaMessage::unknown);
+	public static final Lazy<MiraiLogger> LOGGER = new LazyJVM<>(() -> MiraiLogger.Factory.INSTANCE.create(SupernovaMessage.class));
 	public static final String KEY_TYPE = "type";
 	public static final String KEY_DATA = "data";
 	public static final String KEY_TEXT = "text";
@@ -45,27 +55,8 @@ public class SupernovaMessage implements Message, SingleMessage
 		{
 			Json val = array.get(i);
 			String type = val.string(KEY_TYPE);
-			Json data = val.get(KEY_DATA);
-			if (TYPE_TEXT.equals(type))
-			{
-				PlainText msg = new PlainText(data.string(KEY_TEXT));
-				builder.add(msg);
-			}
-			if (TYPE_IMAGE.equals(type))
-			{
-				SupernovaImage img = new SupernovaImage(data.string(KEY_FILE));
-				img.raw = data.stringify();
-				builder.add(img);
-			}
-			if (TYPE_FACE.equals(type))
-			{
-				Face face = new Face(Integer.parseInt(data.string(KEY_ID)));
-				builder.add(face);
-			}
-			if (TYPE_JSON.equals(type))
-			{
-				builder.add(new LightApp(data.string(KEY_DATA)));
-			}
+			Function<Json, SingleMessage> deserializer = DESERIALIZERS.getOrDefault(type, UNKNOWN_MESSAGE.getValue());
+			builder.add(deserializer.apply(val));
 		}
 		return builder.build();
 	}
@@ -81,5 +72,49 @@ public class SupernovaMessage implements Message, SingleMessage
 	public String toString()
 	{
 		return this.contentToString();
+	}
+
+	public static PlainText text(Json val)
+	{
+		Json data = val.get(KEY_DATA);
+		return new PlainText(data.string(KEY_TEXT));
+	}
+
+	public static SupernovaImage image(Json val)
+	{
+		Json data = val.get(KEY_DATA);
+		SupernovaImage img = new SupernovaImage(data.string(KEY_FILE));
+		img.raw = data.stringify();
+		return img;
+	}
+
+	public static Face face(Json val)
+	{
+		Json data = val.get(KEY_DATA);
+		return new Face(Integer.parseInt(data.string(KEY_ID)));
+	}
+
+	public static LightApp app(Json val)
+	{
+		Json data = val.get(KEY_DATA);
+		return new LightApp(data.string(KEY_DATA));
+	}
+
+	public static UnknownMessage unknown(Json val)
+	{
+		return new UnknownMessage(val);
+	}
+
+	public static void register(String type, Function<Json, SingleMessage> deserializer)
+	{
+		DESERIALIZERS.put(type, deserializer);
+	}
+
+	static
+	{
+		register(TYPE_TEXT, SupernovaMessage::text);
+		register(TYPE_IMAGE, SupernovaMessage::image);
+		register(TYPE_FACE, SupernovaMessage::face);
+		register(TYPE_JSON, SupernovaMessage::app);
 	}
 }
